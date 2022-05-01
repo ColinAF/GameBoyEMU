645ad9869b8e8dd304ea3683391b7faf3bc56d91
Colin Frisch <cafrisch98@gmail.com>
2022-04-30 17:15:19 -0700

    feat: open and read the tittle of a ROM

--- emu/disassembler.py
+++ emu/disassembler.py
@@ -3,19 +3,33 @@ Author: Colin Frisch
 File: disassembler.py
 Purpose: This module will open and disassemble the GB ROM
 """
+import struct
 
-import sys
+from matplotlib.pyplot import title
+
+ROMS_PATH = "../ROMS/"
+ROM_NAME = ROMS_PATH + "snake.gb" # Make name a command line arg
+
+HEADER_START = 0x0100
+HEADER_END = 0x014F 
 
 def load_rom():
-    """ Open the ROM file """
-    pass
+    """ Open the ROM file and break into sections """
+    with open(ROM_NAME, "rb") as rom:
+        data = rom.read()
+    
+    header = data[HEADER_START:HEADER_END]
+    title = struct.unpack_from('>15s', header[0x0034:0x0043])
+    print(title)
+    
 
 def parse_rom():
     """ Parse ROM's machine code into opcodes """
     pass
 
 def disassemble():
-    """ Load in a ROM file and disassemble it """
-    print(sys.byteorder) 
+    """ Load in a ROM file and disassemble it """ 
+    load_rom()
+
 
 disassemble()
